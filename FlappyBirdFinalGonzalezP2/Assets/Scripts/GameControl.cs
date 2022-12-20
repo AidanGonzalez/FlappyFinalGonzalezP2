9073c83be0dba0446ec183d9c4d11f275bf8abab
AidanGonzalez <724624@student.dusd.net>
2022-12-20 10:41:46 -0800

    STUFF

---
+++ FlappyBirdFinalGonzalezP2/Assets/Scripts/GameControl.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    public Text scoreText;
+    public GameObject gameOvertext;
+    private int score = 0;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+     void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this) ;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    public void BirdScored()
+    {
+        if (gameOver)
+        return;
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+    public void BirdDied()
+    {
+        gameOvertext.SetActive(true);
+        gameOver = true;
+    }
+}
